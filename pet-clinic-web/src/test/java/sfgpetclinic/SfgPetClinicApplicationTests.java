1cd5caa1b34ec238d7ccba004b7581fe8fa2ed9f
STABM42 <seid.tabakovic@realdolmen.com>
2018-11-28 22:30:37 +0100

    Implement interfaces for services. Close#5

--- pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,14 +1,13 @@
 package sfgpetclinic;
 
-import org.junit.Ignore;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@Ignore
 public class SfgPetClinicApplicationTests {
 
-    @Ignore
+    @Test
     public void contextLoads() {
     }
 
