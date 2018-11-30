112e295582fe0191d15c37eff1890a876911bf1c
STABM42 <seid.tabakovic@realdolmen.com>
2018-11-30 14:42:51 +0100

    DataLoader issues

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class DataLoader implements CommandLineRunner {
+
+    private final OwnerService ownerService;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
